7474bc2045dfd59b5cd117f21b17d244c69fdef3
Cole Lawrence <cole@colelawrence.com>
2022-09-04 08:03:19 -0400

    Add `match` helper + 0.0.11

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@
 export * from "./internal/invariant";
 export * from "./internal/devStringify";
 export * from "./internal/DevString";
-export * from "./internal/UsrString";
+export * from "./internal/match";
+export * from "./internal/staticCheck";
 export * from "./internal/tightJsonStringify";
 export * from "./internal/tightObjectDebug";
-export * from "./internal/staticCheck";
+export * from "./internal/UsrString";
 // functional pipe used for many sdk builders
 export * from "./internal/pipe";
 // zod for runtime parsing for type validators (accepted and output cannot be different types)
